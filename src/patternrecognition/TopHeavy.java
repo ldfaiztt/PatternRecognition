5a5c9fa4e9990c63ad2a9399cfa39c48384e746f
Lucien Knechtli <dazedy@outlook.com>
2014-01-25 22:53:37 -0500

    implemented most of classifier

--- src/patternrecognition/TopHeavy.java
+++ src/patternrecognition/TopHeavy.java
@@ -26,21 +26,38 @@ public class TopHeavy extends Classifier{
         
     }
     private int topHeavy(Letter data){
-        return 0;
+        int classifier = 0;
+        classifier = (int)((100.0*topArea(data))/bottomArea(data)+0.5);
+        return classifier;
     }
     private int topArea(Letter data){
+        int black = 0;
         int midpoint = data.height/2;
-        
-        return 0;
+        for(int i = 0; i < midpoint; i++){
+            for(int j = 0; j < data.width; j++){
+                black += data.image[i][j];
+            }
+        }
+        return black;
     }
     private int bottomArea(Letter data){
+        int black = 0;
         int midpoint = data.height/2;
-        
-        return 0;
+        for(int i = midpoint; i < data.height; i++){
+            for(int j = 0; j < data.width; j++){
+                black += data.image[i][j];
+            }
+        }
+        return black;
     }
-    private int blackArea(Letter data){
-        
-        return 0;
+    public int blackArea(Letter data){
+        int black = 0;
+        for(int i = 0; i < data.height; i++){
+            for(int j = 0; j < data.width; j++){
+                black += data.image[i][j];
+            }
+        }
+        return black;
     }
     
 }
